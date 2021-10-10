dae541343ab0fea94540075f6e1a39459866b91b
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-10-10 06:08:06 +0530

    add users route for admin

--- routers/admin.router.js
+++ routers/admin.router.js
@@ -4,9 +4,9 @@ const router = Router();
 import { listUsers } from "../controllers/admin.controller";
 import { isAuthenticated, isAdmin } from "../middlewares/auth.middleware";
 
-router.route("/").get(isAuthenticated, isAdmin, listUsers);
+router.route("/users").get(isAuthenticated, isAdmin, listUsers);
 // router
-//   .route("/:id")
+//   .route("user/:id")
 //   .get(isAuthenticated, isAdmin, getUser)
 //   .put(isAuthenticated, isAdmin, updateUser)
 //   .delete(isAuthenticated, isAdmin, deleteUser);
