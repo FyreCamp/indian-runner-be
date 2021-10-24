0e57d374d342b023ea9f4f2845f54c65c3fce53a
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-10-24 23:20:58 +0530

    add info route

--- routers/user.routes.js
+++ routers/user.routes.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { getInfo } from "../controllers/user.controller";
 const router = Router();
 
-router.get("/", getInfo);
+router.get("/info", getInfo);
 
 export default router;
