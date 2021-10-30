e7d9256a300973efbbaaada30e9fbb298d7a1ab3
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-10-31 04:57:59 +0530

    add name and id as select fields

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -24,8 +24,7 @@ export const listChallenges = async (req, res) => {
 export const listMyChallenges = async (req, res) => {
   const myChallenges = await User.find({ _id: req.user._id }).populate({
     path: "challenges",
-    model: "Challenge",
-    select: "title description _id",
+    select: "name _id",
   });
   res.status(200).json({
     status: "success",
